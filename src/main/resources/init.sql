d7109cfc1f89d8bb67f25e76be3811eab15141d8
KatherineSuprun <catesupr@gmail.com>
2023-08-21 12:17:02 +0300

    Commit!

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -11,19 +11,19 @@ INSERT INTO "public".homework (id, username, description) values
 CREATE TABLE IF NOT EXISTS "public".lesson (
         id serial NOT NULL PRIMARY KEY,
         username text NOT NULL UNIQUE,
-        updated_At text NOT NULL,
+        updated_At int NOT NULL,
         homework_id int NOT NULL,
         FOREIGN KEY (homework_id) REFERENCES "public".homework (id)
 );
 
 INSERT INTO "public".lesson (id, username, updated_At, homework_id) values
-       (default, 'lesson1', 'classwork1', 1),
-       (default, 'lesson2', 'classwork2', 1);
+       (default, 'lesson1', 3, 1),
+       (default, 'lesson2', 3, 1);
 
 CREATE TABLE IF NOT EXISTS "public".schedule (
         id serial NOT NULL PRIMARY KEY,
         username text NOT NULL,
-        updated_At text NOT NULL,
+        updated_At int NOT NULL,
         lesson int NOT NULL,
         FOREIGN KEY (username) REFERENCES "public".lesson (username),
         FOREIGN KEY (lesson) REFERENCES "public".lesson (id)
