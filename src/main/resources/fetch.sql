5504ce4293169f9aa3b2257891b5ad957319d8db
KatherineSuprun <catesupr@gmail.com>
2023-08-21 12:57:07 +0300

    Commit!

--- src/main/resources/fetch.sql
+++ src/main/resources/fetch.sql
@@ -8,8 +8,5 @@ SELECT * FROM homework, lesson ORDER BY homework.id, lesson.id ASC;
 SELECT * FROM schedule
 JOIN lesson on schedule.username = lesson.username;
 
-select homework_id AS lesson_username,
-count (schedule.id) AS quantity
-from lesson
-left join schedule on schedule.id = schedule.id and lesson = 2
-group by lesson_username;
+SELECT * FROM schedule sch
+INNER JOIN lesson les ON sch.id = les."id";
